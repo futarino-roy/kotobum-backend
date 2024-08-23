9e99038a47b069a6d3c886fbedd67d55a842074c
futarino-roy <futarino.m+roy@gmail.com>
2024-08-23 11:10:23 +0900

    add auth

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,14 @@ use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\DashboardController;
 
-// Route::prefix('admin')->group(function () {
-//     Route::middleware('auth:admin')->group(function () {
-//         Route::get('/logout', [LoginController::class, 'logout'])->name('admin.logout');
-//         // Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-//         // Route::resource('/users', UserController::class);
-//     });
-//     Route::get('/login', [LoginController::class, 'login_form'])->name('admin.login_form');
-//     Route::post('/login', [LoginController::class, 'login'])->name('admin.login');
-// });
-Route::get('/login', [LoginController::class, 'login_form'])->name('admin.login_form');
-Route::post('/login', [LoginController::class, 'login'])->name('admin.login');
+Route::prefix('admin')->group(function () {
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('/logout', [LoginController::class, 'logout'])->name('admin.logout');
+        // Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+        // Route::resource('/users', UserController::class);
+    });
+    Route::get('/login', [LoginController::class, 'login_form'])->name('admin.login_form');
+    Route::post('/login', [LoginController::class, 'login'])->name('admin.login');
+});
+// Route::get('/login', [LoginController::class, 'login_form'])->name('admin.login_form');
+// Route::post('/login', [LoginController::class, 'login'])->name('admin.login');
