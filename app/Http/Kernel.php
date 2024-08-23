34839d9a4af7f67ba05665d3d2ef7f2837e9f874
futarino-roy <futarino.m+roy@gmail.com>
2024-08-23 11:18:59 +0900

    add adminauth and kernel shusei

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'auth.original_admins' => \App\Http\Middleware\OriginalAdminAuth::class,
+        'auth.admin' => \App\Http\Middleware\AdminAuth::class,
     ];
 }
