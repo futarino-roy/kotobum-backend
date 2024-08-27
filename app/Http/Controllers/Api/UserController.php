129e2614f42f164994d3064037c65c92db02b277
futarino-roy <futarino.m+roy@gmail.com>
2024-08-27 10:31:45 +0900

    add name api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller; 
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
     public function show(Request $request)
     {
-        return response()->json($request->user());
+        // ユーザー情報を取得
+        $user = $request->user();
+
+        // 名前を取得
+        $name = $user->name;
+
+        // 名前をJSON形式で返す
+        return response()->json(['name' => $name]);
     }
 }
--- database/migrations/2024_08_27_094032_add_columns_to_albums_table.php
+++ database/migrations/2024_08_27_094032_add_columns_to_albums_table.php
@@ -4,29 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddColumnsToAlbumsTable extends Migration
 {
     public function up(): void
     {
         Schema::table('albums', function (Blueprint $table) {
-            $table->longText('cover')->nullable()->change();
-            $table->longText('body')->nullable()->change();
-            $table->unsignedBigInteger('user_id')->after('body');
-            $table->boolean('is_sent')->default(false)->after('user_id');
-            
-            // 外部キー制約を追加
+            $table->unsignedBigInteger('user_id')->after('id');
+            $table->string('cover')->nullable()->after('user_id');
+            $table->text('body')->nullable()->after('cover');
+            $table->boolean('is_sent')->default(false)->after('body');
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('albums', function (Blueprint $table) {
-            $table->text('cover')->nullable()->change();
-            $table->text('body')->nullable()->change();
             $table->dropForeign(['user_id']);
-            $table->dropColumn(['user_id', 'is_sent']);
+            $table->dropColumn(['user_id', 'cover', 'body', 'is_sent']);
         });
     }
-};
+}
+
