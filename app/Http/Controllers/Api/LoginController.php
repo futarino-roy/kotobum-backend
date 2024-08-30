c398554294227bcb64b28aed86711608657d7969
futarino-roy <futarino.m+roy@gmail.com>
2024-08-30 10:51:01 +0900

    modify login api1

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Sanctum\PersonalAccessToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
-    {
+    public function login(Request $request){
         // バリデーション
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
@@ -25,14 +25,13 @@ class LoginController extends Controller
 
         // ユーザー認証
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            $user = Auth::user(); // ユーザー情報を取得
-            $user->tokens()->delete(); // 既存のトークンを削除
+            $user = User::where('email', $request->email)->first();
+            $user->tokens()->delete();
             $token = $user->createToken("login:user{$user->id}")->plainTextToken;
 
-            // `template`情報を含めてレスポンスを返す
             return response()->json([
                 'token' => $token,
-                'template' => $user->template // ユーザーの`template`を追加
+                'template' => $user->template
             ], Response::HTTP_OK);
         }
 
