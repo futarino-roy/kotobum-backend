8becba4aff4fe86dd9cbcc93323574980622bfc8
Kondoreiya <futarino.m2@gmail.com>
2024-10-29 08:52:16 +0900

    album_id

--- app/Http/Controllers/Api/BodyController.php
+++ app/Http/Controllers/Api/BodyController.php
@@ -29,9 +29,9 @@ class BodyController extends Controller
         ]);  
 
         // ユーザーの権限をチェック
-        if (/* $album->user_id !== auth()->id() || */ $album->is_sent) {
+        /* if ($album->user_id !== auth()->id() || $album->is_sent) {
             return response()->json(['message' => 'Unauthorized or already sent'], 403);
-        }
+        } */
 
          
 
