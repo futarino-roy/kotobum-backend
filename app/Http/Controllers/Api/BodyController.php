89ec25910f03375ff4e482cee8ef02816e949752
Kondoreiya <futarino.m2@gmail.com>
2024-10-29 16:05:00 +0900

    フロントエンド側の型修正

--- app/Http/Controllers/Api/BodyController.php
+++ app/Http/Controllers/Api/BodyController.php
@@ -19,11 +19,11 @@ class BodyController extends Controller
         /* Log::info('Album Data:', $album->toArray());
         Log::info('Album ID:', ['id' => $album->id]); 
         Log::info('Request Data:', $request->all()); */
-        $request->validate([
-            'textData' => 'required|JSON',
-            'imageData' => 'required|JSON',
-            'colors' => 'required|JSON',
-        ]);  
+        /* $request->validate([
+            'textData' => 'required|array',
+            'imageData' => 'required|array',
+            'colors' => 'required|array',
+        ]);   */
 
         $album = Album::findOrFail($albumid);
 
