b4aafca1c581cc859fa03f243ed0091003b3b623
Kondoreiya <futarino.m2@gmail.com>
2024-11-15 13:58:52 +0900

    PDF化機能仮実装”

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ Route::prefix('admin')->group(function () {
         Route::get('/logout', [LoginController::class, 'logout'])->name('admin.logout'); // ログアウト / Logout
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
-        Route::get('/coverHTML{userid}', [PDFController::class, 'coverHTML'])->name('admin.coverHTML'); 
-        Route::get('/bodyHTML{userid}', [PDFController::class, 'bodyHTML'])->name('admin.bodyHTML'); 
+        Route::get('/coverHTML/{userid}', [PDFController::class, 'coverHTML'])->name('admin.coverHTML'); 
+        Route::get('/bodyHTML/{userid}', [PDFController::class, 'bodyHTML'])->name('admin.bodyHTML'); 
        /*  Route::post('/PDF', [PDFController::class, 'PDF'])->name('admin.PDF'); 
         Route::get('ユーザー情報（所属・ステ・関連アルバムID）取得', [::class, ''])->name('');
         Route::get('ユーザー情報（所属個別テーブル？）取得', [::class, ''])->name('');
