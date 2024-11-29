beef0089488a7e37230a5bcd457db71dcdcd5280
Kondoreiya <futarino.m2@gmail.com>
2024-11-29 13:47:18 +0900

    HTML処理とパートナー処理

--- database/migrations/2024_11_29_110519_covertext_cover.php
+++ database/migrations/2024_11_29_110519_covertext_cover.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('cover', function (Blueprint $table) {
+        Schema::table('covers', function (Blueprint $table) {
             //
             $table->integer('covertext')->nullable();
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('cover', function (Blueprint $table) {
+        Schema::table('covers', function (Blueprint $table) {
             //
             $table->dropColumn('covertext');
         });
