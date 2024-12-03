1c77ec67fdab3e6204965a5bcc0ed134ac6f8885
Kondoreiya <futarino.m2@gmail.com>
2024-12-03 10:13:06 +0900

    パートナー画面

--- resources/views/admin/partner.blade.php
+++ resources/views/admin/partner.blade.php
@@ -53,6 +53,35 @@
         </tbody>
     </table>
 
+    <h2 style="margin-left:10%;">他ユーザー</h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>A面B面</th>
+                <th>ID</th>
+                <th>名前</th>
+                <th>表紙校了</th>
+                <th>中身校了</th>
+                <th>パートナー</th>
+                <th>割り当て</th>
+                <th>ページ移動</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                @if ($user->id == $A->id || $user->id == $B->id)
+                    @continue
+                @endif
+        <tr>
+            <td>{{ $user->template }}</td>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->cover_is_send }}</td>
+            <td>{{ $user->body_is_send }}</td>
+            <td><a href="{{ route('admin.showPartner', $A->partner_id) }}">割り当て</a></td>
+            <td><a href="{{ route('admin.showPartner', $A->partner_id) }}">移動</a></td>
+        </tr>
+    @endforeach
     
 @endsection
 
