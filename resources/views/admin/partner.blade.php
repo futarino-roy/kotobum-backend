240d9d317adbcfef6fda59f61689a4fedd85d90f
Kondoreiya <futarino.m2@gmail.com>
2024-12-03 10:19:26 +0900

    パートナー画面

--- resources/views/admin/partner.blade.php
+++ resources/views/admin/partner.blade.php
@@ -15,7 +15,7 @@
                 <th>名前</th>
                 <th>表紙校了</th>
                 <th>中身校了</th>
-                <th>結びつけ削除</th>
+                <th>結びつけ解除</th>
             </tr>
         </thead>
         <tbody>
@@ -23,8 +23,8 @@
                     <td>A</td>
                     <td>{{ $A->id ?? 'N/A'}}</td>
                     <td>{{ $A->name ?? 'N/A'}}</td>
-                    <td>{{ $A->album->cover_is_send ?? 'N/A'}}</td>
-                    <td>{{ $A->album->body_is_send ?? 'N/A'}}</td>
+                    <td>{{ $A->album->cover_is_sent ?? 'N/A'}}</td>
+                    <td>{{ $A->album->body_is_sent ?? 'N/A'}}</td>
                     <td>@if(is_null($A->partner_id))
                          <!-- partner_idがnullの場合、リンクを無効にしてスタイル変更 -->
                          <a href="#" style="pointer-events: none; color: gray;">結びつけ解除不可</a>
@@ -39,8 +39,8 @@
                     <td>B</td>
                     <td>{{ $B->id ?? 'N/A'}}</td>
                     <td>{{ $B->name ?? 'N/A'}}</td>
-                    <td>{{ $B->album->cover_is_send ?? 'N/A'}}</td>
-                    <td>{{ $B->album->body_is_send ?? 'N/A'}}</td>
+                    <td>{{ $B->album->cover_is_sent ?? 'N/A'}}</td>
+                    <td>{{ $B->album->body_is_sent ?? 'N/A'}}</td>
                     <td> @if(is_null($A->partner_id))
                          <!-- partner_idがnullの場合、リンクを無効にしてスタイル変更 -->
                          <a href="#" style="pointer-events: none; color: gray;">結びつけ解除不可</a>
@@ -53,7 +53,35 @@
         </tbody>
     </table>
 
-    
+    <h2 style="margin-left:10%;">他ユーザー</h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>A面B面</th>
+                <th>ID</th>
+                <th>名前</th>
+                <th>表紙校了</th>
+                <th>中身校了</th>
+                <th>パートナー</th>
+                <th>割り当て</th>
+                <th>ページ移動</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                @if ($user->id == $A->id || $user->id == $B->id)
+                    @continue
+                @endif
+        <tr>
+            <td>{{ $user->template }}</td>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->cover_is_send }}</td>
+            <td>{{ $user->body_is_send }}</td>
+            <td><a href="{{ route('admin.showPartner', $A->partner_id) }}">割り当て</a></td>
+            <td><a href="{{ route('admin.showPartner', $A->partner_id) }}">移動</a></td>
+        </tr>
+    @endforeach
 
 @endsection
 
