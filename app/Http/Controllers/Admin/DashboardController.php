6b29c7cd60cd79b241c8068e403b19683c150121
Kondoreiya <futarino.m2@gmail.com>
2024-12-03 14:24:06 +0900

    パートナー画面

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
     {
         // ログインしている管理者を取得
         $admin = Auth::guard('admin')->user();
-        $users = User::all();
+        $users = User::with('partner')->get();
         
         // ダッシュボードビューを返す
         return view('admin.dashboard', compact('admin','users'));
