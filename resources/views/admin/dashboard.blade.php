5281a3bd9616a378292d09c78c2dc8e80896b45d
Kondoreiya <futarino.m2@gmail.com>
2024-12-10 10:11:06 +0900

    ボディのみPDF化

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,8 +14,8 @@
                 <th>名前</th>
                 <th>ログインID</th>
                 <th>A面B面</th>
-                <!-- <th>パートナー</th> -->
-                <!-- <th>フタリノ状況</th> -->
+                <th>パートナー</th>
+                <th>フタリノ状況</th>
                 <th>作成日</th>
                 <!-- <th>Cover編集（仮）</th>
                 <th>Body編集（仮）</th> -->
@@ -30,8 +30,8 @@
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->login_id }}</td>
                     <td>{{ $user->template }}</td>
-                    <!-- <td>{{ optional($user->partner)->name }}</td> -->
-                    <!-- <td><a href="{{ route('admin.showPartner', $user->id) }}">フタリノ画面</a></td> -->
+                    <td>{{ optional($user->partner)->name }}</td>
+                    <td><a href="{{ route('admin.showPartner', $user->id) }}">フタリノ画面</a></td>
                     <td>{{ $user->created_at }}</td>
                     <!-- <td><a href="{{ route('admin.coverHTML', $user->id) }}">PDF</a></td> -->
                     <td><a href="{{ route('admin.bodyHTML', $user->id) }}">PDF</a></td>
