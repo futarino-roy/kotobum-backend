26e99dab37b411ef192071d94d3f20fcb496bba5
Kondoreiya <futarino.m2@gmail.com>
2024-12-12 17:00:12 +0900

    ボディのみPDF化

--- resources/views/pdf/edit.blade.php
+++ resources/views/pdf/edit.blade.php
@@ -22,7 +22,7 @@
      <button type="submit">PDFを生成</button>
     </form>
 
-    <<img src="{{ asset('img/kotobum_format1/t1/p1@2x.jpg') }}" alt="" style="width: 100%; max-width: 600px" />
+    <img src="{{ asset('img/kotobum_format1/t1/p1@2x.jpg') }}" alt="" >
 
     <h3>プレビューエリア</h3>
     <iframe id="preview"></iframe>
