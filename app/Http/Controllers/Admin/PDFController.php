d078072f0b59b39a4d915dfe482808c9d4e9f388
Kondoreiya <futarino.m2@gmail.com>
2024-12-17 09:50:00 +0900

    ボディのみPDF化-レイアウト

--- app/Http/Controllers/Admin/PDFController.php
+++ app/Http/Controllers/Admin/PDFController.php
@@ -129,7 +129,7 @@ class PDFController extends Controller
         $htmlContent = $request->input('html_content');
 
         $mpdf = new \Mpdf\Mpdf([
-            /* 'format' => array( 158, 218), */
+            'format' => array( 158, 218),
         ]); //サイズ指定 カバー335、250　ボディ158、218
 
         // HTMLをPDFに変換
