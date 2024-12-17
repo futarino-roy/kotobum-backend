c68c0e9eed303979f8072ee4b6fb211ffa479024
Kondoreiya <futarino.m2@gmail.com>
2024-12-17 09:30:55 +0900

    ボディのみPDF化-レイアウト

--- config/pdf.php
+++ config/pdf.php
@@ -2,7 +2,7 @@
 
 return [
     'mode'                     => 'ja',
-    'format'                   => 'A5',
+    'format'                   => '',
     'default_font_size'        => '12',
     'default_font'             => 'sans-serif',
     'margin_left'              => 0,
