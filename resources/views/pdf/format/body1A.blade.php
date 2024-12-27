43091850185018d6779d1e26f46f64c30af84b9a
Kondoreiya <futarino.m2@gmail.com>
2024-12-27 16:14:36 +0900

    ボディのみPDF化-レイアウト

--- resources/views/pdf/format/body1A.blade.php
+++ resources/views/pdf/format/body1A.blade.php
@@ -32,10 +32,10 @@
 <body style="background-color:{{$colors['backgroundColor'] ?? '#000000' }}; text-align:center;">
   <img src="{{ asset('img/kotobum_format1/2-12@2x.png') }}" alt="" style="width: 158mm; height:218mm;"/>
   <div id="textArea24-1" style="position:absolute; width:{{$textData[0]['width']??'50'}}pt; height:{{$textData[0]['height']??'50'}}pt; top:{{$textData[0]['top']??'50'}}pt; left:{{$textData[0]['left']??'50'}}pt; text-orientation:upright; font-size:13pt; line-height:1; font-family:'League Spartan',sans-serif; text-align:center; font-weight: bold; color:{{$colors['textColor'] ?? '#000000'}};">{{$textData[0]['text'] ?? ''}}</div>
-  <div id="frontimg24-1" class="img-radius" style="position:absolute; width:{{$imageData[0]['width']??'50'}}pt; height:{{$imageData[0]['height']??'50'}}pt; top:{{$imageData[0]['top']??'50'}}pt; left:{{$imageData[0]['left']??'50'}}pt;"><img src="{{$imageData[0]['image'] }}" style="width:100%; height:100%;"></div>
-  <div id="frontimg24-2" class="img-radius" style="position:absolute; width:{{$imageData[1]['width']??'50'}}pt; height:{{$imageData[1]['height']??'50'}}pt; top:{{$imageData[1]['top']??'50'}}pt; left:{{$imageData[1]['left']??'50'}}pt;"><img src="{{$imageData[1]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontimg24-1" class="img-radius" style="position:absolute; width:{{$imageData[0]['width']??'50'}}pt; height:{{$imageData[0]['height']??'50'}}pt; top:{{$imageData[0]['top']??'50'}}pt; left:{{$imageData[0]['left']??'50'}}pt;"><img src="{{$imageData[0]['image'] }}" style="width:{{$imageData[0]['width']??'50'}}pt; height:{{$imageData[0]['height']??'50'}}pt;"></div>
+  <div id="frontimg24-2" class="img-radius" style="position:absolute; width:{{$imageData[1]['width']??'50'}}pt; height:{{$imageData[1]['height']??'50'}}pt; top:{{$imageData[1]['top']??'50'}}pt; left:{{$imageData[1]['left']??'50'}}pt;"><img src="{{$imageData[1]['image'] }}" style="width:{{$imageData[1]['width']??'50'}}pt; height:{{$imageData[1]['height']??'50'}}pt;"></div>
   <div id="textArea24-2" style="position:absolute; width:{{$textData[1]['width']??'50'}}pt; height:{{$textData[1]['height']??'50'}}pt; top:{{$textData[1]['top']??'50'}}pt; left:{{$textData[1]['left']??'50'}}pt; writing-mode:vertical-rl; font-size:7.5pt; text-orientation:upright;">{{$textData[1]['text'] ?? ''}}</div>
-  <div id="frontimg24-3" class="img-radius" style="position:absolute; width:{{$imageData[2]['width']??'50'}}pt; height:{{$imageData[2]['height']??'50'}}pt; top:{{$imageData[2]['top']??'50'}}pt; left:{{$imageData[2]['left']??'50'}}pt;"><img src="{{$imageData[2]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontimg24-3" class="img-radius" style="position:absolute; width:{{$imageData[2]['width']??'50'}}pt; height:{{$imageData[2]['height']??'50'}}pt; top:{{$imageData[2]['top']??'50'}}pt; left:{{$imageData[2]['left']??'50'}}pt;"><img src="{{$imageData[2]['image'] }}" style="width:{{$imageData[2]['width']??'50'}}pt; height:{{$imageData[2]['height']??'50'}}pt;"></div>
   <div id="textArea24-4" style="position:absolute; width:{{$textData[2]['width']??'50'}}pt; height:{{$textData[2]['height']??'50'}}pt; top:{{$textData[2]['top']??'50'}}pt; left:{{$textData[2]['left']??'50'}}pt; text-orientation:upright; text-align:center; line-height:1.5; font-weight:bold; color:#fef8f6; font-size:12.3pt; ">{{$textData[2]['text'] ?? ''}}</div>
 
     
@@ -50,7 +50,7 @@
 
   <img src="{{ asset('img/kotobum_format1/2-10@2x.png') }}" alt="" style="width: 158mm; height:218mm;" />
   <div id="textArea22" style="position:absolute; width:{{$textData[5]['width']??'50'}}pt; height:{{$textData[5]['height']??'50'}}pt; top:{{$textData[5]['top']??'50'}}pt; left:{{$textData[5]['left']??'50'}}pt; color:{{$colors['textColor'] ?? '#000000'}}; font-size:14pt; line-height: 1.5; text-align:center; font-weight:bold; writing-mode:vertical-rl; text-orientation:upright;">{{$textData[5]['text'] ?? ''}}</div>
-  <div id="frontimg22" class="img-radius" style="position:absolute; width:{{$imageData[3]['width']??'50'}}pt; height:{{$imageData[3]['height']??'50'}}pt; top:{{$imageData[3]['top']??'50'}}pt; left:{{$imageData[3]['left']??'50'}}pt;"><img src="{{$imageData[3]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontimg22" class="img-radius" style="position:absolute; width:{{$imageData[3]['width']??'50'}}pt; height:{{$imageData[3]['height']??'50'}}pt; top:{{$imageData[3]['top']??'50'}}pt; left:{{$imageData[3]['left']??'50'}}pt;"><img src="{{$imageData[3]['image'] }}" style="width:{{$imageData[3]['width']??'50'}}pt; height:{{$imageData[3]['height']??'50'}}pt;"></div>
 
   <pagebreak />
 
@@ -62,7 +62,7 @@
 
   <img src="{{ asset('img/kotobum_format1/2-8@2x.png') }}" alt="" style="width: 158mm; height:218mm;" />
   <div id="textArea20" style="position:absolute; width:{{$textData[7]['width']??'50'}}pt; height:{{$textData[7]['height']??'50'}}pt; top:{{$textData[7]['top']??'50'}}pt; left:{{$textData[7]['left']??'50'}}pt; font-size:14pt; line-height:1.5; font-weight:bold; text-align:center; color:#fef8f6; text-orientation:upright;">{{$textData[7]['text'] ?? ''}}</div>
-  <div id="frontimg20" class="img-radius" style="position:absolute; width:{{$imageData[4]['width']??'50'}}pt; height:{{$imageData[4]['height']??'50'}}pt; top:{{$imageData[4]['top']??'50'}}pt; left:{{$imageData[4]['left']??'50'}}pt;"><img src="{{$imageData[4]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontimg20" class="img-radius" style="position:absolute; width:{{$imageData[4]['width']??'50'}}pt; height:{{$imageData[4]['height']??'50'}}pt; top:{{$imageData[4]['top']??'50'}}pt; left:{{$imageData[4]['left']??'50'}}pt;"><img src="{{$imageData[4]['image'] }}" style="width:{{$imageData[4]['width']??'50'}}pt; height:{{$imageData[4]['height']??'50'}}pt;"></div>
 
   <pagebreak />
 
@@ -73,7 +73,7 @@
 
   <img src="{{ asset('img/kotobum_format1/2-6@2x.png') }}" alt="" style="width: 158mm; height:218mm;" />
   <div id="textArea18" style="position:absolute; width:{{$textData[9]['width']??'50'}}pt; height:{{$textData[9]['height']??'50'}}pt; top:{{$textData[9]['top']??'50'}}pt; left:{{$textData[9]['left']??'50'}}pt; font-size:14pt; line-height: 1.5; text-align:center; font-weight:bold; writing-mode:vertical-rl; text-orientation:upright;">{{$textData[9]['text'] ?? ''}}</div>
-  <div id="frontimg18" class="img-radius" style="position:absolute; width:{{$imageData[5]['width']??'50'}}pt; height:{{$imageData[5]['height']??'50'}}pt; top:{{$imageData[5]['top']??'50'}}pt; left:{{$imageData[5]['left']??'50'}}pt;"><img src="{{$imageData[5]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontimg18" class="img-radius" style="position:absolute; width:{{$imageData[5]['width']??'50'}}pt; height:{{$imageData[5]['height']??'50'}}pt; top:{{$imageData[5]['top']??'50'}}pt; left:{{$imageData[5]['left']??'50'}}pt;"><img src="{{$imageData[5]['image'] }}" style="width:{{$imageData[5]['width']??'50'}}pt; height:{{$imageData[5]['height']??'50'}}pt;"></div>
 
   <pagebreak />
 
@@ -84,7 +84,7 @@
 
   <img src="{{ asset('img/kotobum_format1/1-4@2x.png') }}" alt="" style="width: 158mm; height:218mm;" />
   <div id="textArea16-1" style="position:absolute; width:{{$textData[11]['width']??'50'}}pt; height:{{$textData[11]['height']??'50'}}pt; top:{{$textData[11]['top']??'50'}}pt; left:{{$textData[11]['left']??'50'}}pt; font-size:16pt; line-height:1.5; font-weight:bold; text-align:center; color:#fef8f6; text-orientation:upright;">{{$textData[11]['text'] ?? ''}}</div>
-  <div id="frontung16" style="position:absolute; width:{{$imageData[6]['width']??'50'}}pt; height:{{$imageData[6]['height']??'50'}}pt; top:{{$imageData[6]['top']??'50'}}pt; left:{{$imageData[6]['left']??'50'}}pt;"><img src="{{$imageData[6]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontung16" style="position:absolute; width:{{$imageData[6]['width']??'50'}}pt; height:{{$imageData[6]['height']??'50'}}pt; top:{{$imageData[6]['top']??'50'}}pt; left:{{$imageData[6]['left']??'50'}}pt;"><img src="{{$imageData[6]['image'] }}" style="width:{{$imageData[6]['width']??'50'}}pt; height:{{$imageData[6]['height']??'50'}}pt;"></div>
   <div id="textArea16-2" style="position:absolute; width:{{$textData[12]['width']??'50'}}pt; height:{{$textData[12]['height']??'50'}}pt; top:{{$textData[12]['top']??'50'}}pt; left:{{$textData[12]['left']??'50'}}pt; font-size:14pt; line-height:1.5; font-weight:bold; text-align:center; color:#fef8f6; text-orientation:upright;">{{$textData[12]['text'] ?? ''}}</div>
 
   <pagebreak />
@@ -97,14 +97,14 @@
 
   <img src="{{ asset('img/kotobum_format1/1-2@2x.png') }}" alt="" style="width: 158mm; height:218mm;" />
   <div id="textArea14" style="position:absolute; width:{{$textData[14]['width']??'50'}}pt; height:{{$textData[14]['height']??'50'}}pt; top:{{$textData[14]['top']??'50'}}pt; left:{{$textData[14]['left']??'50'}}pt; color:{{$colors['textColor'] ?? '#000000'}}; font-size:16pt; line-height: 1.5; text-align:center; font-weight:bold; writing-mode:vertical-rl; text-orientation:upright;">{{$textData[14]['text'] ?? ''}}</div>
-  <div id="frontimg14" class="img-radius" style="position:absolute; width:{{$imageData[7]['width']??'50'}}pt; height:{{$imageData[7]['height']??'50'}}pt; top:{{$imageData[7]['top']??'50'}}pt; left:{{$imageData[7]['left']??'50'}}pt;"><img src="{{$imageData[7]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontimg14" class="img-radius" style="position:absolute; width:{{$imageData[7]['width']??'50'}}pt; height:{{$imageData[7]['height']??'50'}}pt; top:{{$imageData[7]['top']??'50'}}pt; left:{{$imageData[7]['left']??'50'}}pt;"><img src="{{$imageData[7]['image'] }}" style="width:{{$imageData[7]['width']??'50'}}pt; height:{{$imageData[7]['height']??'50'}}pt;"></div>
 
   <pagebreak />
 
   <img src="{{ asset('img/kotobum_format1/1-1@2x.png') }}" alt="" style="width: 158mm; height:218mm;" />
   <div id="textArea13-1" style="position:absolute; width:{{$textData[15]['width']??'50'}}pt; height:{{$textData[15]['height']??'50'}}pt; top:{{$textData[15]['top']??'50'}}pt; left:{{$textData[15]['left']??'50'}}pt; color:{{$colors['textColor'] ?? '#000000'}}; font-family:'League Spartan', sans-serif; margin:30px 0 0 0; font-size:28.6pt; line-height:1.2; font-weight:bold; text-align:center;">{{$textData[15]['text'] ?? ''}}</div>
   <div id="textArea13-2" style="position:absolute; width:{{$textData[16]['width']??'50'}}pt; height:{{$textData[16]['height']??'50'}}pt; top:{{$textData[16]['top']??'50'}}pt; left:{{$textData[16]['left']??'50'}}pt; font-family:'League Spartan', sans-serif; font-size:12pt; text-align:center; line-height:1.5; font-weight:bold;">{{$textData[16]['text'] ?? ''}}</div>
-  <div id="frontimg13" class="img-radius" style="position:absolute; width:{{$imageData[8]['width']??'50'}}pt; height:{{$imageData[8]['height']??'50'}}pt; top:{{$imageData[8]['top']??'50'}}pt; left:{{$imageData[8]['left']??'50'}}pt;"><img src="{{$imageData[8]['image'] }}" style="width:100%; height:100%;"></div>
+  <div id="frontimg13" class="img-radius" style="position:absolute; width:{{$imageData[8]['width']??'50'}}pt; height:{{$imageData[8]['height']??'50'}}pt; top:{{$imageData[8]['top']??'50'}}pt; left:{{$imageData[8]['left']??'50'}}pt;"><img src="{{$imageData[8]['image'] }}" style="width:{{$imageData[8]['width']??'50'}}pt; height:{{$imageData[8]['height']??'50'}}pt;"></div>
   <div id="textArea13-3" style="position:absolute;width:{{$textData[17]['width']??'50'}}pt; height:{{$textData[17]['height']??'50'}}pt; top:{{$textData[17]['top']??'50'}}pt; left:{{$textData[17]['left']??'50'}}pt; font-size:7.5pt; writing-mode:vertical-rl; text-orientation:upright;">{{$textData[17]['text'] ?? ''}}</div>
 
 </body>
