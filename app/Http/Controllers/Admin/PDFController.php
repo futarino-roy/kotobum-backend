73b90eca16c9c93d1526983a7942f96d58fe2e3a
Kondoreiya <futarino.m2@gmail.com>
2024-12-27 11:23:50 +0900

    ボディのみPDF化-レイアウト

--- app/Http/Controllers/Admin/PDFController.php
+++ app/Http/Controllers/Admin/PDFController.php
@@ -151,7 +151,7 @@ class PDFController extends Controller
         $mpdfConfig = config('pdf');
 
         $customConfig = array_merge($mpdfConfig, [
-            'format' => [160, 225] //サイズ指定 カバー335、250　ボディ158、218
+            'format' => [158, 220] //サイズ指定 カバー335、250　ボディ158、218
         ]);
 
         $mpdf = new MpdfMpdf($customConfig); 
