12f8a0715a1b24fdee3c6ba2aebe2465cbc4812c
Kondoreiya <futarino.m2@gmail.com>
2025-01-17 13:53:24 +0900

    フォント文字化け

--- app/Http/Controllers/Admin/PDFController.php
+++ app/Http/Controllers/Admin/PDFController.php
@@ -169,6 +169,7 @@ class PDFController extends Controller
 
         $mpdf = new MpdfMpdf($customConfig); 
         $mpdf->BMPonly[] = 'notosansjp';
+        $mpdf->SetDefaultFont = 'notosansjp'
 
         /* dump($mpdfConfig);
         dump($customConfig); */
