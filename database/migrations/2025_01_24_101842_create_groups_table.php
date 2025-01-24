23d48890cccd24bc399ab9de26f3aabd527d2048
Kondoreiya <futarino.m2@gmail.com>
2025-01-24 14:57:34 +0900

    フォーマット修正

--- database/migrations/2025_01_24_101842_create_groups_table.php
+++ database/migrations/2025_01_24_101842_create_groups_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('format');
             $table->unsignedBigInteger('Auser_id')->nullable();
             $table->unsignedBigInteger('Buser_id')->nullable();
             $table->timestamps();
