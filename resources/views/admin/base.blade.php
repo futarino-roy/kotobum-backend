fad87c9488abdb764e9fe8ef172e98e8706d4948
Kondoreiya <futarino.m2@gmail.com>
2025-03-10 17:43:57 +0900

    管理画面ルーティング整理

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -66,6 +66,12 @@
             </div>
         @endif -->
 
+        @if(session('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -113,8 +119,4 @@
 </body>
 </html>
 
-@if(session('message'))
-    <div class="alert alert-success">
-        {{ session('message') }}
-    </div>
-@endif
+
