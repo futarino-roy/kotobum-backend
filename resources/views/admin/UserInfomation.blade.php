62d372f54d7e2419290a8358fbe0b8b306c7bbd1
Kondoreiya <futarino.m2@gmail.com>
2025-03-19 10:13:19 +0900

    baceページ変更

--- resources/views/admin/UserInfomation.blade.php
+++ resources/views/admin/UserInfomation.blade.php
@@ -61,7 +61,7 @@
             <tr>
                 <th><a href="{{ route('admin_user_redirect', ['userid' => $user->id, 'parts' => 'body']) }}" target="_blank">中身編集ページ</a></th>
                 <th>
-                    <a class="reset-status-btn btn btn-warning"
+                    <a class="btn btn-warning"
                         href="{{ route('admin.delete_user', ['userid' => $user->id]) }}"
                         onclick="return confirm('本当に削除しますか？この操作は取り消せません。')"
                         style="color: red;">データ削除
