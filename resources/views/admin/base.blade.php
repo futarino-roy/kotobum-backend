c17c232957562f3df344a9b8711dca66e2522030
Kondoreiya <futarino.m2@gmail.com>
2025-03-31 02:45:48 +0900

    パスワードリセット

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'デフォルトタイトル')</title>
     <link rel="stylesheet" href="{{ asset('assets/css/base.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
